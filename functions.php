9e238c4b2b9b72601aa4e2a5fc7499d92b0246f8
Erik <ej.erik.jimenez@gmail.com>
2026-05-14 01:23:37 -0500

    api function call

--- functions.php
+++ functions.php
@@ -34,4 +34,29 @@ function validateSerialNumber(&$prefix, &$body, $serialNumber) : bool {
 
       return false;
 }
+
+function callApi($apiUrl, $payload, $method) {
+   json_encode($payload);
+   $ch = curl_init($apiUrl);
+
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+   if($payload) {
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+   }
+   
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content_type: application/json',
+      'Content_length: ' . strlen($payload)));
+
+   $res = curl_exec($ch);
+   curl_close($ch);
+
+   $data = json_decode($res, true);
+
+   return $data;
+}
 ?>
